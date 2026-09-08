ied206/scummvm-zoombini

ZOOMBINI2: Implement Zoombins Mountain Rescue engine

engines/zoombini2/module.mk
@@ -1,0 +1,50 @@
+MODULE := engines/zoombini2
+
+MODULE_OBJS := \
+	game_state.o \
+	gfx.o \
+	help_screen.o \
+	metaengine.o \
+	path.o \
+	sidebar.o \
+	sound.o \
+	ui.o \
+	pages/page.o \
+	pages/video.o \
+	pages/title.o \
+	pages/zombiniville.o \
+	pages/maptrans.o \
+	pages/puzzle.o \
+	pages/aquacube.o \
+	pages/booliewood.o \
+	pages/boolies.o \
+	pages/cheznorf.o \
+	pages/crazyturtle.o \
+	pages/magicwall.o \
+	pages/mysticmarsh.o \
+	pages/snowboard.o \
+	pages/waterslide.o \
+	pages/walloffleens.o \
+	pages/worldmap.o \
+	pages/save_file_list.o \
+	pages/menuscreen.o \
+	pages/rescue.o \
+	pages/credits.o \
+	pages/final.o \
+	zoombini.o \
+	zoombini2.o
+
+MODULE_DIRS += \
+	engines/zoombini2 \
+	engines/zoombini2/pages
+
+# This module can be built as a plugin
+ifeq ($(ENABLE_ZOOMBINI2), DYNAMIC_PLUGIN)
+PLUGIN := 1
+endif
+
+# Include common rules
+include $(srcdir)/rules.mk
+
+# Detection objects
+DETECT_OBJS += $(MODULE)/detection.o

ied206/scummvm

engines/zoombini2/module.mk
@@ -1,0 +1,53 @@
+MODULE := engines/zoombini2
+
+MODULE_OBJS := \
+	dialogs.o \
+	game_state.o \
+	gfx.o \
+	pages/dialog_help.o \
+	metaengine.o \
+	path.o \
+	saveload.o \
+	sidebar.o \
+	sound.o \
+	ui.o \
+	pages/page_base.o \
+	pages/transition_video.o \
+	pages/transition_title.o \
+	pages/shelter_zombiniville.o \
+	pages/transition_maptrans.o \
+	pages/transition_maptrans_route.o \
+	pages/puzzle_base.o \
+	pages/puzzle_aquacube.o \
+	pages/shelter_booliewood.o \
+	pages/puzzle_boolies.o \
+	pages/puzzle_cheznorf.o \
+	pages/puzzle_crazyturtle.o \
+	pages/puzzle_magicwall.o \
+	pages/puzzle_mysticmarsh.o \
+	pages/puzzle_snowboard.o \
+	pages/puzzle_waterslide.o \
+	pages/puzzle_walloffleens.o \
+	pages/interactive_worldmap.o \
+	pages/save_file_list.o \
+	pages/interactive_menuscreen.o \
+	pages/shelter_rescue.o \
+	pages/transition_credits.o \
+	pages/transition_final.o \
+	zoombini.o \
+	zoombini2.o
+
+MODULE_DIRS += \
+	engines/zoombini2 \
+	engines/zoombini2/pages
+
+# This module can be built as a plugin
+ifeq ($(ENABLE_ZOOMBINI2), DYNAMIC_PLUGIN)
+PLUGIN := 1
+endif
+
+# Include common rules
+include $(srcdir)/rules.mk
+
+# Detection objects
+DETECT_OBJS += $(MODULE)/detection.o